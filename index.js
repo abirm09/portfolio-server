48632de918514f0bf25709bb9fa5c41f83fc04ac
Md. Abir Mahmud <abirmahmud5665@gmail.com>
2023-06-29 22:58:45 +0600

    implemented contact api

--- index.js
+++ index.js
@@ -1,16 +1,49 @@
 const express = require("express");
 const cors = require("cors");
-const PORT = process.env.PORT || 5000;
 const app = express();
-
-// middleware
+const port = process.env.PORT || 5000;
+require("dotenv").config();
 app.use(cors());
 app.use(express.json());
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const exp = require("constants");
+const uri =
+  "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.9.1";
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.v6yry4e.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    client.connect();
+    const messagesCollection = client.db("portfolio").collection("messages");
+    app.post("/message", async (req, res) => {
+      const body = req.body;
+      const { email, subject, message } = body;
+      const data = { email, subject, message };
+      const result = await messagesCollection.insertOne(data);
+      res.send(result);
+    });
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send(["Server is running."]);
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is started at port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is started at port ${port}`);
 });
